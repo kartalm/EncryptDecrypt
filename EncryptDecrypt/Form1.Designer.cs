fec5410b8f05715e76d924d9ec603c187e39dbc3
Mehmet KARTAL <mehmetbariskartal@gmail.com>
2018-02-12 09:22:04 +0300

    First Commit

---
+++ EncryptDecrypt/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace EncryptDecrypt
+{
+    partial class frmEncryptDecrypt
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecript = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.RichTextBox();
+            this.txtOutput = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(12, 154);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(75, 72);
+            this.btnEncrypt.TabIndex = 0;
+            this.btnEncrypt.Text = "Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnDecript
+            // 
+            this.btnDecript.Location = new System.Drawing.Point(148, 154);
+            this.btnDecript.Name = "btnDecript";
+            this.btnDecript.Size = new System.Drawing.Size(75, 72);
+            this.btnDecript.TabIndex = 1;
+            this.btnDecript.Text = "Decript";
+            this.btnDecript.UseVisualStyleBackColor = true;
+            this.btnDecript.Click += new System.EventHandler(this.btnDecript_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(277, 154);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 72);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(12, 12);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(340, 136);
+            this.txtInput.TabIndex = 3;
+            this.txtInput.Text = "";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(12, 232);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(340, 127);
+            this.txtOutput.TabIndex = 4;
+            this.txtOutput.Text = "";
+            // 
+            // frmEncryptDecrypt
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(364, 371);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnDecript);
+            this.Controls.Add(this.btnEncrypt);
+            this.Name = "frmEncryptDecrypt";
+            this.Text = "Encrypt Decrypt";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecript;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.RichTextBox txtInput;
+        private System.Windows.Forms.RichTextBox txtOutput;
+    }
+}
+
---
+++ EncryptDecrypt/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EncryptDecrypt
+{
+    public partial class frmEncryptDecrypt : Form
+    {
+        public frmEncryptDecrypt()
+        {
+            InitializeComponent();
+        }
+
+        public static String Encrypt(string clearText)
+        {
+            clearText = clearText.Trim();
+            const string encryptionKey = "MAKV2SPBNI99212";
+            byte[] clearBytes = Encoding.Unicode.GetBytes(clearText);
+            using (Aes encryptor = Aes.Create())
+            {
+                var pdb = new Rfc2898DeriveBytes(encryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(clearBytes, 0, clearBytes.Length);
+                        cs.Close();
+                    }
+                    clearText = Convert.ToBase64String(ms.ToArray());
+                }
+            }
+            return clearText;
+        }
+
+        public static String Decrypt(string cipherText)
+        {
+            cipherText = cipherText.Trim();
+            const string EncryptionKey = "MAKV2SPBNI99212";
+            byte[] cipherBytes = Convert.FromBase64String(cipherText);
+            using (Aes encryptor = Aes.Create())
+            {
+                var pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(cipherBytes, 0, cipherBytes.Length);
+                        cs.Close();
+                    }
+                    cipherText = Encoding.Unicode.GetString(ms.ToArray());
+                }
+            }
+            return cipherText;
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtInput.Text))
+            {
+                txtOutput.Text = string.Empty;
+
+                try
+                {
+                    txtOutput.Text = Encrypt(txtInput.Text.Trim());
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(@"Unable to encrypt text. Error : " + ex);
+                }
+            }
+            else
+            {
+                MessageBox.Show(@"Please enter any text!");
+            }
+        }
+
+        private void btnDecript_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtInput.Text))
+            {
+                txtOutput.Text = string.Empty;
+
+                try
+                {
+                    txtOutput.Text = Decrypt(txtInput.Text.Trim());
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(@"Unable to decrypt text. Error : " + ex);
+                }
+            }
+            else
+            {
+                MessageBox.Show(@"Please enter any text!");
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtInput.Text = string.Empty;
+            txtOutput.Text = string.Empty;
+        }
+
+    }
+}
